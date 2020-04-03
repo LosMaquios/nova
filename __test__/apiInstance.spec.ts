f5f533262c064e28137db15bf78b5a60f6c7a8ee
Camilo Rodríguez <aeroxmotion@hotmail.com>
2020-04-03 13:14:17 -0500

    Fix isntance test

--- __test__/apiInstance.spec.ts
+++ __test__/apiInstance.spec.ts
@@ -4,19 +4,21 @@ import {
 } from '../src'
 
 describe('api: instance', () => {
-  test('get instance', async () => {
+  test('get instance', done => {
     function TestElementInstance () {
       const instance = getElementInstance()
 
       expect(typeof instance.__id).toBe('string')
       expect(instance.__constructor).toBe(TestElementInstance)
+
+      done()
     }
 
     defineElement(TestElementInstance)
-
     document.createElement('test-element-instance')
-    await customElements.whenDefined('test-element-instance')
+  })
 
+  test('wrong get instance call', () => {
     // wrong instance call
     expect(() => getElementInstance()).toThrowError('Unknown instance')
   })
