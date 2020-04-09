2804b9ca7f0aad2a33fc87fdd500b71bf6acdce7
Camilo Rodríguez <aeroxmotion@hotmail.com>
2020-04-09 15:00:19 -0500

    Expose watcher internals

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,7 @@ export * from './apiComposers'
 
 // Registration API
 export * from './apiDefine'
+
+// Expose internal watcher utils
+export { WatcherHandler } from './WatcherHandler'
+export { WatcherCollection } from './WatcherCollection'
